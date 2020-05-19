4a11c49a3fa416561c32a4800fc129061ca33ce9
batsaikhan84 <u.batsaikhan@gmail.com>
2020-05-18 22:30:14 -0500

    user input change

--- cli_gem_project/lib/cli.rb
+++ cli_gem_project/lib/cli.rb
@@ -1,4 +1,8 @@
 class CLI
+  
+  attr_accessor :input, :input_again
+  @@input = ""
+  @@input_again = ""
 
   MAIN_URL = "https://www.scholarships.com/financial-aid/college-scholarships/scholarship-directory/academic-major"
 
@@ -6,20 +10,24 @@ class CLI
     academic_majors
     puts "Welcome to Scholarship finder!"
     puts "\n"
-    input = ""
-    while input != "exit" || input_again != "exit"
+    puts "You can type 'exit' anytime to exit."
+    
+    while @@input != "Exit"
       puts "Please enter academic major for the list of scholarship"
-      input = gets.strip
-      if input == "exit"
+      
+      @@input = gets.strip.capitalize
+      if @@input == "Exit"
+        puts "Thank You! Have a great day."
         break
       end
-      scholarship_finder(input)
+      scholarship_finder(@@input)
       puts "Please choose a number to see the details of scholarship"
-      input_again = gets.strip
-      if input_again == "exit"
+      @@input_again = gets.strip.capitalize
+      if @@input_again == "Exit"
+        puts "Thank You! Have a great day."
         break
       end
-      detail(input, input_again)
+      detail(@@input, @@input_again)
     end
   end
 
@@ -34,18 +42,30 @@ class CLI
   end
 
   def scholarship_finder(input)
+    while Scholarship.all.detect {|e| e.major == input} == nil
+      puts "Please enter valid entry!"
+      input = gets.strip.capitalize
+      @@input = input
+    end
     url = Scholarship.all.detect {|e| e.major == input}.major_url
     i = 0
     title_and_detail(url).each do |e|
-         i += 1
-         puts "#{i}. #{e[:title]} - #{e[:amount]}"
+      i += 1
+      puts "#{i}. #{e[:title]} - #{e[:amount]}"
     end
   end
 
   def detail(input, input_again)
     url = Scholarship.all.detect {|e| e.major == input}.major_url
+    while !input_again.to_i.between?(1, title_and_detail(url).length + 1)
+      puts "Please enter number between 1 and #{title_and_detail(url).length}"
+      input_again = gets.strip.capitalize
+      @@input_again = input_again
+    end
     detail_url = title_and_detail(url)[input_again.to_i - 1][:scholarship_url]
     Scraper.detail(detail_url)
+    
   end
-
+  
 end
+
